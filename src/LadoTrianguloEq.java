a6af6bd1c6817bf72afdbadb5ac4ee08cfe2aae5
jjairocas <jjcastan@unal.edu.co>
2023-08-30 13:16:22 -0500

    Programa sencillo donde se calcula el area, el perimetro y la altura de un triangulo equilatero teniendo un solo lado

---
+++ src/LadoTrianguloEq.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class LadoTrianguloEq {
+    public static void main(String[] args) {
+        Scanner input=new Scanner((System.in));
+        System.out.println("Ingrese el lado del triangulo: ");
+        double a= input.nextDouble();
+        double h=(a*Math.sqrt(3))/2;
+
+        double Atriangulo=(a*h)/2;
+        double PerimetroTriangulo=(a*3);
+
+        System.out.println("El perimetro del triangulo es: "+PerimetroTriangulo);
+        System.out.println("La altura del triangulo es: "+h);
+        System.out.println("El area del triangulo es: "+Atriangulo);
+    }
+}
